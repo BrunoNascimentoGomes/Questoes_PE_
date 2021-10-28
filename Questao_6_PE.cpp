fe6b3a26617370c9da89a46404803dc9883695c1
BrunoNascimentoGomes <93275117+BrunoNascimentoGomes@users.noreply.github.com>
2021-10-28 01:32:20 -0300

    Update Questao_6_PE.cpp

--- Questao_6_PE.cpp
+++ Questao_6_PE.cpp
@@ -12,17 +12,17 @@ int main(){
     int aux = 0;
     int soma_da_dif;
     
-    for(int i = 1; i <= 100; i = i + 1){
-        soma_quadrados = soma_quadrados + pow(i,2);
+    for(int i = 1; i <= 100; i = i + 1){                        //Faz a soma dos quadrados
+        soma_quadrados = soma_quadrados + pow(i,2); 
     }
     
-    for(int x = 1; x<=100 ; x = x + 1){
+    for(int x = 1; x<=100 ; x = x + 1){                        //Faz a soma de 1 até 100
         aux = aux + x;
     }
     
-    quadrado_soma = pow(aux,2);
+    quadrado_soma = pow(aux,2);                               //Faz o quadrado da soma
     
-    soma_da_dif = quadrado_soma - soma_quadrados;
+    soma_da_dif = quadrado_soma - soma_quadrados;             //Calcula a diferença do quadrado da soma e da soma dos quadrados
     
     cout<<soma_da_dif<<endl;
 

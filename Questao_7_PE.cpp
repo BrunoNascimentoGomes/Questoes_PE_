4e7101dfafae78da98a4f4872c32502e2da1fd45
BrunoNascimentoGomes <93275117+BrunoNascimentoGomes@users.noreply.github.com>
2021-10-28 01:35:57 -0300

    Update Questao_7_PE.cpp

--- Questao_7_PE.cpp
+++ Questao_7_PE.cpp
@@ -17,17 +17,17 @@ int main(){
     
     
     while(num){
-        for(int a = 1; a <= num; a = a + 1){
+        for(int a = 1; a <= num; a = a + 1){     
             if(num%a == 0){
                 cont = cont + 1;
             }
         }
-        if(cont == 2){
+        if(cont == 2){                          //Esse for e if testam se um número é primo e adicionam esse valor no vetor primo
             primo.push_back(num);
 
         }
         cont = 0;
-        tam = primo.size();
+        tam = primo.size();                   //Calcula o tamanho do vetor e testa quando chegamos ao tamanho 10001
         
         if(tam == 10001){
             break;
@@ -35,7 +35,7 @@ int main(){
         num = num + 1;
     }
     
-    cout<<primo[10000]<<endl;
+    cout<<primo[10000]<<endl;            //Pega a última posição do vetor que seria o 10001° número primo
   
 
     return 0;

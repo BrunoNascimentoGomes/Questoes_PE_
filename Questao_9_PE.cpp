0e8e7e7b718f43359e009bab4cbf69e2fe4f5695
BrunoNascimentoGomes <93275117+BrunoNascimentoGomes@users.noreply.github.com>
2021-10-28 01:45:07 -0300

    Update Questao_9_PE.cpp

--- Questao_9_PE.cpp
+++ Questao_9_PE.cpp
@@ -12,10 +12,10 @@ int main(){
     long long int c;
     long long int x;
     
-    for(a = 1 ;a < 1000; a++){
-        for( b = 1; b < 1000; b++){
-            c = (1000 - a - b);
-            if(c*c == a*a + b*b){
+    for(a = 1 ;a < 1000; a++){                //Como a não pode ser maior que mil, pois a+b+c=1000
+        for( b = 1; b < 1000; b++){           //Mesma explicação do primeiro for
+            c = (1000 - a - b);               //Acha o valor de c para todos os casos de a e b de 1 até 1000
+            if(c*c == a*a + b*b){             //Faz o teste para printar a multiplicação
                 cout<<a*b*c<<endl;
                 
              }

25be3fb9cd1976ce44f9f4c1ece679bc720bd6a8
BrunoNascimentoGomes <93275117+BrunoNascimentoGomes@users.noreply.github.com>
2021-10-28 01:15:39 -0300

    Update Questao_1_PE.cpp

--- Questao_1_PE.cpp
+++ Questao_1_PE.cpp
@@ -9,23 +9,23 @@ int main()
     int y = 0;
     int z = 0;
     
-    for (int i = 3; i < 1000; i = i + 3){
+    for (int i = 3; i < 1000; i = i + 3){       //Soma tds os multiplos de 3 até 1000
         
         x  = x + i;
         
     }
-    for (int i = 5; i < 1000; i = i + 5){
+    for (int i = 5; i < 1000; i = i + 5){       //Soma tds os multiplos de 5 até 1000
         
         y  = y + i;
         
     }
-    for (int i = 15; i < 1000; i = i + 15){
+    for (int i = 15; i < 1000; i = i + 15){    //Soma tds os multiplos de 3 e 5 ao mesmo tempo até 1000
         
         z  = z + i;
         
     }
     
-    cout<<x+y-z<<endl;
+    cout<<x+y-z<<endl;              //Soma os múltiplos de 3 e 5 e exclui os que são múltiplos dos dois ao mesmo tempo
     return 0;
 }
        

c7e8cb76072e8ec5b0f42a9c8a217ec31387f642
BrunoNascimentoGomes <93275117+BrunoNascimentoGomes@users.noreply.github.com>
2021-10-28 01:30:53 -0300

    Update Questao_5_PE.cpp

--- Questao_5_PE.cpp
+++ Questao_5_PE.cpp
@@ -11,10 +11,10 @@ int main(){
     
     while(1){
         i = i + 1;
-        if(i%10 == 0){
-            if(i%11 == 0){
-                if(i%12 == 0){
-                    if(i%13 == 0){
+        if(i%10 == 0){                                         //Como eu sei q todos os números primos de 1 até 20, estão presentes de 10 até 20, 
+            if(i%11 == 0){                                     //eu posso testar se "i" é divisível de 10 até 20, se n for, ele soma um ao valor de "i"
+                if(i%12 == 0){                                 //e faz o teste novamente.
+                    if(i%13 == 0){                             
                         if(i%14 == 0){
                             if(i%15 == 0){
                                 if(i%16 == 0){
